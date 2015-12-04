3f9121938952b81c825356ece078dbb087c1f2bf
Roni Choudhury <roni.choudhury@kitware.com>
2015-12-04 15:19:07 -0500

    Red/blue colormap for propagandist vs. audience

--- src/js/index.js
+++ src/js/index.js
@@ -117,11 +117,8 @@ $(function () {
                 return d.data.usernames && d.data.usernames[0] || "Group node";
             },
             fill: function (d) {
-                // Red for inactive users (e.g., mentioned by others only),
-                // purple for active users with non-geolocated tweets, blue for
-                // active users with geolocated tweets, and brown for group
-                // nodes.
-                return d.data.grouped ? "987654" : (!d.data.active ? "#ca0020" : (d.data.geolocated ? "#0571b0" : "#7b3294"));
+                // Red for propagandists; blue for audience.
+                return d.data.type === "propagandist" ? "#ef8a62" : "#67a9cf";
             },
             nodeRadius: function (d, r) {
                 return d.data && d.data.grouped ? 2*r : r;
